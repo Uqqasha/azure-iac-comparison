3ab77c7939871f8c4d2e345bf9f897854c03c5ec
Uqqasha Ijaz <uqqasha@c-05b7e655.041-79-73746f1.bbcust.telenor.se>
2024-04-09 18:15:46 +0200

    Updated provisioning commands

--- ARM/test/commands.sh
+++ ARM/test/commands.sh
@@ -1,2 +1,2 @@
-cmd_provision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -f 01-vnet-shared.json -p 01-vnet-shared.parameters.json --verbose"
+cmd_provision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -f root.json -p root.parameters.json --verbose"
 cmd_unprovision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -f cleanup.json --mode complete --verbose"
