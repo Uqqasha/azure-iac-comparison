d04cfa34460da944bfd9901f78702f9693363f6b
Uqqasha Ijaz <uqqasha@c-f963e655.034-343-73746f1.bbcust.telenor.se>
2024-06-01 13:47:12 +0200

    Changed Bicep unprovision command to use ARM unprovision

--- bicep/test/commands.sh
+++ bicep/test/commands.sh
@@ -1,2 +1,2 @@
 cmd_provision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -p root.bicepparam --verbose"
-cmd_unprovision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -f cleanup.bicep --mode complete --verbose"
+cmd_unprovision="az deployment group create -g RG-${region}-${iac_tool}-${execution} -f cleanup.json --mode complete --verbose"
